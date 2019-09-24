57972d69d4cb5e55920eb5d3c6b143dd12122e2b
jazmin10 <jazminestrada10@gmail.com>
2019-09-24 10:53:54 -0500

    initial files

---
+++ index.js
@@ -0,0 +1,35 @@
+class TodoManager {
+
+    constructor(todos) {
+
+    }
+}
+
+
+//sample usage
+const initialTodos = [{
+    title: "Learn JavaScript",
+    is_completed: true
+}, {
+    title: "Practice flashcards",
+    is_completed: false
+}];
+const todoManager = new TodoManager(initialTodos);
+
+console.log(todoManager.getCompleted()); //returns completed todos
+console.log(todoManager.getPending()); //returns todos that are not completed
+console.log(todoManager.getFirst());
+console.log(todoManager.getLast());
+console.log(todoManager.getCount());
+todoManager.markAsCompleted("Practice flashcards"); //receives a title, finds todo with that title and marks it as completed
+todoManager.add({
+    title: "Recap flashcards",
+    is_completed: true
+});
+//when is_completed is not defined, it should default to false
+todoManager.add({
+    title: "Get some rest"
+});
+console.log(todoManager.allCompleted()); //returns true when all todo items are completed
+console.log(todoManager.allPending()); //returns true when all todo items are pending
+console.log(todoManager.exportCsv()); //returns Comma Separated Values of titles "Todo title, Another todo title"
